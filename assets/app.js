00e7dbe5037c253350a5eaf043f3ca1486d6bb7d
Patrick Geys <patrick.geys2@telenet.be>
2026-05-11 18:38:00 +0200

    Scherem aanpassen iPhone

--- assets/app.js
+++ assets/app.js
@@ -799,17 +799,21 @@ function renderGroupedCards(items) {
 }
 
 function renderCircuitTabs() {
-  const baseData = getVisibleData();
-
-  const dataForTabs = baseData.filter(matchesChip);
+  const baseData = (Array.isArray(DATA) ? DATA : []).filter(item => {
+    if (item.deleted) return false;
+    return matchesFilter(item, activeChip);
+  });
 
   const countFor = (key) => {
     if (key === "alles") {
-      return dataForTabs.length;
+      return baseData.length;
     }
 
-    return dataForTabs.filter(x => (x.circuit || "pc") === key).length;
+    return baseData.filter(x => (x.circuit || "pc") === key).length;
   };
+
+  // rest van je bestaande code laten staan
+
 
 const tabs = [
   { key: "alles", label: "Alle circuits" },
